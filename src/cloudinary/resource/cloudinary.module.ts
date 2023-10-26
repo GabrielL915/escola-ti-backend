73aa78d9f55e9ff3ffaea1cb231338340d7c269c
gabriel gomes <gabrielluizgomes1@gmail.com>
2023-10-26 20:28:01 -0300

    implementação testes unitarios do cloudinary

--- src/cloudinary/resource/cloudinary.module.ts
+++ src/cloudinary/resource/cloudinary.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CloudinaryUseCase } from '../domain/use-cases/cloudinary.use-case';
 import { CloudinaryProvider } from '../data-access/infraestructure/storage/cloudinary.provider';
-import { UploadController } from './controler';
 import { ICloudinaryProvider } from '../domain/interfaces/icloudinary.provider';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [ConfigModule],
-  controllers: [UploadController],
+  controllers: [],
   providers: [
     CloudinaryUseCase,
     {
