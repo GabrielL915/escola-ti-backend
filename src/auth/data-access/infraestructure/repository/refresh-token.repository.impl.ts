a2efa6a299d4b1f1a371f62969199523fe89feb5
gabriel gomes <gabrielluizgomes1@gmail.com>
2023-11-12 18:43:18 -0300

    implementação de novos teste unitarios para auth

--- src/auth/data-access/infraestructure/repository/refresh-token.repository.impl.ts
+++ src/auth/data-access/infraestructure/repository/refresh-token.repository.impl.ts
@@ -6,45 +6,31 @@ import { InjectKnex } from 'nestjs-knex';
 export class RefreshTokenRepositoryImpl implements RefreshTokenRepository {
   constructor(@InjectKnex() private readonly knex: Knex) {}
 
+  //remover
   async createAccount(
     id: string,
     accessToken: string,
     refreshToken: string,
   ): Promise<void> {
-    try {
-      await this.knex('conta')
-        .insert({
-          id_entregador: id,
-          access_token: accessToken,
-          refresh_token: refreshToken,
-        })
-        .returning('id');
-    } catch (error) {
-      throw new InternalServerErrorException('Erro ao salvar tokens no banco', error);
-    }
+    await this.knex('conta')
+      .insert({
+        id_entregador: id,
+        access_token: accessToken,
+        refresh_token: refreshToken,
+      })
+      .returning('id');
   }
 
   async getStoredTokens(id: string): Promise<any> {
-    try {
-      return await this.knex
-        .from('conta')
-        .select('refresh_token')
-        .where({ id_entregador: id });
-    } catch (error) {
-      throw new InternalServerErrorException(
-        'Erro ao recuperar tokens do banco.',
-        error,
-      );
-    }
+    return await this.knex
+      .from('conta')
+      .select('refresh_token')
+      .where({ id_entregador: id });
   }
 
   async updateAccount(id: string, accessToken: string, refreshToken: string) {
-    try {
-      return await this.knex('conta')
-        .where({ id_entregador: id })
-        .update({ refresh_token: refreshToken, access_token: accessToken });
-    } catch (error) {
-      throw new InternalServerErrorException('Erro ao atualizar conta no banco.', error);
-    }
+    return await this.knex('conta')
+      .where({ id_entregador: id })
+      .update({ refresh_token: refreshToken, access_token: accessToken });
   }
 }
